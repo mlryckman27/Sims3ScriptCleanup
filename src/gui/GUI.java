bf2f1e7251979c2fe01d284884baec620a0c860d
mlryc <mlryckman27@gmail.com>
2022-10-09 18:37:03 -0400

    Changed GUI class to inherit from Frame.  This utilizes inheritance instead of association when making a GUI window.

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -3,19 +3,30 @@ package gui;
 import javax.swing.*;
 import java.awt.*;
 
-public class GUI {
+public class GUI extends Frame {
 
     public GUI() {
-        JFrame frame = new JFrame();
+        // panel = new JPanel();
+        // panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        //panel.setLayout(new GridLayout(0, 1));
 
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
+        TextField textField1 = new TextField();
+        textField1.setBounds(50, 50, 150, 25);
+        textField1.setEditable(true);
 
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("test0");
-        frame.pack();
-        frame.setVisible(true);
+        //private JPanel panel = null;
+        Button cleanButton = new Button();
+        cleanButton.setBounds(200, 200, 50, 25);
+
+        //add(panel);
+        add(cleanButton); add(textField1);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setTitle("test0");
+
+        setSize(600, 480);
+        textField1.setVisible(true);
+        cleanButton.setVisible(true);
+        setVisible(true);
     }
 }
