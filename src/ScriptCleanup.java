2b813ee63533ba8c1383be567f79730463d302e4
mlryc <mlryckman27@gmail.com>
2022-10-10 23:35:52 -0400

    Removed unnecessary test code from main class.

--- src/ScriptCleanup.java
+++ src/ScriptCleanup.java
@@ -8,9 +8,6 @@ public class ScriptCleanup {
     public static void main(String[] args) throws IOException {
         GUI testGUI = new GUI();
         ConfigReader testConfigReader = new ConfigReader();
-
-        System.out.println("This is a test...");
-
     }
 }
 
